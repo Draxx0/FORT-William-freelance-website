449a1d95bf7d9cacc1e0581482cbaa02e9ea9a78
Draxx0 <williamfort.lmgl@gmail.com>
2025-09-18 19:56:22 +0200

    :lipstick: update website solution card

--- src/components/sections/home/solution.tsx
+++ src/components/sections/home/solution.tsx
@@ -10,34 +10,9 @@ const ease = [0.16, 1, 0.3, 1];
 
 import { Icons } from '@/components/icons';
 import { BentoCard, BentoGrid } from '@/components/magicui/bento-grid';
-import Marquee from '@/components/magicui/marquee';
+import Safari from '@/components/safari';
 import { AutomationBean } from '@/components/sections/services/automatisation/automation-bean';
-import { ThreeDMarquee } from '@/components/ui/3d-marquee';
 import { Globe, Share2Icon } from 'lucide-react';
-
-const images = [
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-  '/vitrine-banner.jpg',
-];
 
 const features = [
   {
@@ -48,12 +23,13 @@ const features = [
     cta: 'Commencer',
     className: 'col-span-4 lg:col-span-2',
     background: (
-      <Marquee
-        pauseOnHover
-        className="absolute top-0 [--duration:60s] [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] "
-      >
-        <ThreeDMarquee className="" images={images} />
-      </Marquee>
+      <div className="max-h-[400px] min-h-auto rounded-md shadow-md relative">
+        <Safari
+          url="https://williamfort.fr"
+          src="/vitrine-banner.jpg"
+          className="absolute right-2 top-4 sm:top-8 h-[240px] sm:h-[300px] w-full border-none transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_10%,#000_100%)] group-hover:scale-105"
+        />
+      </div>
     ),
   },
   // {
@@ -115,37 +91,6 @@ export default function ServicesHome() {
       description="Je vous accompagne dans la création de solutions digitales efficaces, adaptées à vos besoins."
       className="bg-neutral-100 dark:bg-neutral-900"
     >
-      {/* <div className="mx-auto mt-16 grid max-w-sm grid-cols-1 gap-6 text-gray-500 md:max-w-3xl md:grid-cols-2 xl:grid-rows-2 md:grid-rows-3 xl:max-w-6xl xl:auto-rows-fr xl:grid-cols-3">
-        {features.map((feature, index) => (
-          <motion.div
-            key={index}
-            className={cn(
-              'group relative items-start overflow-hidden bg-neutral-50 dark:bg-neutral-800 p-6 rounded-2xl',
-              feature.className
-            )}
-            initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{
-              duration: 0.5,
-              type: 'spring',
-              stiffness: 100,
-              damping: 30,
-              delay: index * 0.1,
-            }}
-            viewport={{ once: true }}
-          >
-            <div>
-              <h3 className="font-semibold mb-2 text-primary">
-                {feature.title}
-              </h3>
-              <p className="text-foreground">{feature.description}</p>
-            </div>
-            {feature.content}
-            <div className="absolute bottom-0 left-0 h-32 w-full bg-gradient-to-t from-neutral-50 dark:from-neutral-900 pointer-events-none"></div>
-          </motion.div>
-        ))}
-      </div> */}
-
       <BentoDemo />
 
       <motion.div
